cf14c9fa04f8f9158f1a019ca8a9d2bab4b9f992
Kakon Ray <thisiskakonray@gmail.com>
2022-02-13 12:10:50 +0600

    Completed Shoping Cart

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ const incrementBtn = document.getElementById("incrementbtn");
 const decrementbtn1 = document.getElementById("decrementbtn1");
 const incrementbtn1 = document.getElementById("incrementbtn1");
 
+let subTotalDollerAllItem = document.getElementById("subTotalDollerAllItem");
+const totalDollerAllItem = document.getElementById("totalDollerAllItem");
+const vat = document.getElementById("vat");
+
 decrementbtn.addEventListener("click", () => {
   let inputValue = input.value - 1;
-  if (inputValue > 0) {
+  if (inputValue >= 0) {
     input.value = inputValue;
   }
 
@@ -21,8 +25,20 @@ decrementbtn.addEventListener("click", () => {
   let dollerinput = Number(doller.innerText);
 
   let totalDoller = dollerinput - dollertak;
-  if (totalDoller >= dollertak) {
+  if (totalDoller >= 0) {
     doller.innerText = totalDoller;
+
+    // SubTotal update
+    let inTotaldoller1InnerText = Number(subTotalDollerAllItem.innerText);
+    subTotalDollerAllItem.innerText = inTotaldoller1InnerText - dollertak;
+    // vat update
+    let AllItemVat = Number(vat.innerText);
+    vat.innerText = AllItemVat - 2;
+
+    // Total Doller Update vat + Subtotal
+    const num = Number(subTotalDollerAllItem.innerText);
+    const num2 = Number(vat.innerText);
+    totalDollerAllItem.innerText = num + num2;
   }
 });
 incrementBtn.addEventListener("click", () => {
@@ -33,22 +49,46 @@ incrementBtn.addEventListener("click", () => {
   dollertak = 200;
   let totalDoller = dollertak * inputValue;
   doller.innerText = totalDoller;
+
+  // SubTotal update
+  let inTotaldoller1InnerText = Number(subTotalDollerAllItem.innerText);
+  subTotalDollerAllItem.innerText = inTotaldoller1InnerText + dollertak;
+  // vat update
+  let AllItemVat = Number(vat.innerText);
+  vat.innerText = AllItemVat + 2;
+
+  // Total Doller Update vat + Subtotal
+  const num = Number(subTotalDollerAllItem.innerText);
+  const num2 = Number(vat.innerText);
+  totalDollerAllItem.innerText = num + num2;
 });
 
 // item two
 
 decrementbtn1.addEventListener("click", () => {
   let inputValue = input1.value - 1;
-  if (inputValue > 0) {
+  if (inputValue >= 0) {
     input1.value = inputValue;
   }
 
-  let dollertak = 200;
+  let dollertak = 100;
   let dollerinput = Number(doller1.innerText);
 
   let totalDoller = dollerinput - dollertak;
-  if (totalDoller >= dollertak) {
+  if (totalDoller >= 0) {
     doller1.innerText = totalDoller;
+
+    // SubTotal update
+    let inTotaldoller1InnerText = Number(subTotalDollerAllItem.innerText);
+    subTotalDollerAllItem.innerText = inTotaldoller1InnerText - dollertak;
+    // vat update
+    let AllItemVat = Number(vat.innerText);
+    vat.innerText = AllItemVat - 2;
+
+    // Total Doller Update vat + Subtotal
+    const num = Number(subTotalDollerAllItem.innerText);
+    const num2 = Number(vat.innerText);
+    totalDollerAllItem.innerText = num + num2;
   }
 });
 incrementbtn1.addEventListener("click", () => {
@@ -56,7 +96,19 @@ incrementbtn1.addEventListener("click", () => {
   inputValue = inputValue + 1;
   input1.value = inputValue;
 
-  dollertak = 200;
+  dollertak = 100;
   let totalDoller = dollertak * inputValue;
   doller1.innerText = totalDoller;
+
+  // SubTotal update
+  let inTotaldoller1InnerText = Number(subTotalDollerAllItem.innerText);
+  subTotalDollerAllItem.innerText = inTotaldoller1InnerText + dollertak;
+  // vat update
+  let AllItemVat = Number(vat.innerText);
+  vat.innerText = AllItemVat + 2;
+
+  // Total Doller Update vat + Subtotal
+  const num = Number(subTotalDollerAllItem.innerText);
+  const num2 = Number(vat.innerText);
+  totalDollerAllItem.innerText = num + num2;
 });
